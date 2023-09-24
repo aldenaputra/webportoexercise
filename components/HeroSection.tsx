dc7f0c779cd493f5e285fbd7db85d328353fa2ea
aldenaputra <alden.aputra@gmail.com>
2023-09-25 00:14:01 +0700

    change link JSX

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -22,9 +22,7 @@ const HeroSection = () => {
                 </p>
                 <Link
                 to="projects"
-                className={
-                    "text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
-                }
+                className="text-neutral-100 font-semibold px-6 py-3 bg-teal-600 rounded shadow hover:bg-teal-700"
                 activeClass="active"
                 spy={true}
                 smooth={true}
