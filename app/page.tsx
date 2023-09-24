1cfae0335fa9bfb62853e645e879e7fbb06a8bb4
aldenaputra <alden.aputra@gmail.com>
2023-09-24 18:48:46 +0700

    homepage init

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Image from 'next/image'
-import { Inter } from '@next/font/google'
-import styles from '../styles/page.module.css'
-
-const inter = Inter({ subsets: ['latin'] })
-
+import HeroSection from "@/components/HeroSection"
 export default function Home() {
   return (
-    <main>
+    <main className="mx-auto max-w-3xl px-4 sm:px-6 md:max-w-5xl">
+      <HeroSection />
+
       
     </main>
   )
