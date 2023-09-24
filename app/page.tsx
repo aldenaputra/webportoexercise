081c9c25da99af233d03b0e4208df846c8b17070
aldenaputra <alden.aputra@gmail.com>
2023-09-24 17:21:58 +0700

    navbar d/l mode

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,7 @@ const inter = Inter({ subsets: ['latin'] })
 export default function Home() {
   return (
     <main>
-      <h1 className='text-2xl'>
-        Hello World!
-      </h1>
+      
     </main>
   )
 }
