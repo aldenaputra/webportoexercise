f93270a17438cae4749ac7730f663e44b474baf8
aldenaputra <alden.aputra@gmail.com>
2023-10-21 22:53:18 +0700

    fixing projects offset scrollink

--- components/ProjectSection.tsx
+++ components/ProjectSection.tsx
@@ -74,55 +74,57 @@ const projects = [
 const ProjectSection = () => {
     return (
         <section id="projects">
-            <h1 className='my-10 text-center font-bold text-4xl'>
-                Projects
-                <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
-            </h1>
+            <div className='my-12 pb-12 md:pt-16'>
+                <h1 className='my-10 text-center font-bold text-4xl'>
+                    Projects
+                    <hr className='w-6 h-1 mx-auto my-4 bg-teal-500 border-0 rounded'></hr>
+                </h1>
 
-            <div className='flex flex-col space-y-28'>
-                {projects.map((project, idx)=>{
-                    return(
-                    <div key={idx}>
-                        <SlideUp offset="-300px 0px -300px 0px">
-                            <div className='flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12'>
-                                <div className='mt-8 md:w-1/2'>
-                                    <Link href={project.link} target="_blank">
-                                        <Image
-                                        src={project.image}
-                                        alt=""
-                                        width={1000}
-                                        height={1000}
-                                        className="rounded-xl shadow-xl hover:opacity-70"
-                                        />
-                                    </Link>
-                                </div>
-                                
-                                <div className='mt-8 md:w-1/2'>
-                                    <h1 className='text-3xl font-bold mb-6'>{project.name}</h1>
-                                    <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400'>{project.desc}</p>
-
-                                    <div className='flex flex-row align-bottom space-x-4'>
-                                        <div className='flex flex-wrap flex-row justify-start'>
-                                            {project.techstack.map((tech, techidx)=> {
-                                                return(
-                                                    <p key={techidx} className="bg-gray-200 px-3 py-1.5 mb-1 mr-1 text-teal-600 text-sm rounded-full font-semibold"> {tech} </p>
-                                                )
-                                            })}
-                                        </div>
-
+                <div className='flex flex-col space-y-28'>
+                    {projects.map((project, idx)=>{
+                        return(
+                        <div key={idx}>
+                            <SlideUp offset="-300px 0px -300px 0px">
+                                <div className='flex flex-col animate-slideUpCubiBezier animation-delay-2 md:flex-row md:space-x-12'>
+                                    <div className='mt-8 md:w-1/2'>
                                         <Link href={project.link} target="_blank">
-                                            <BsArrowUpRightSquare
-                                            size={30}
-                                            className="hover:-translate-y-1 transition-transform cursor-pointer"
+                                            <Image
+                                            src={project.image}
+                                            alt=""
+                                            width={1000}
+                                            height={1000}
+                                            className="rounded-xl shadow-xl hover:opacity-70"
                                             />
                                         </Link>
                                     </div>
+                                    
+                                    <div className='mt-8 md:w-1/2'>
+                                        <h1 className='text-3xl font-bold mb-6'>{project.name}</h1>
+                                        <p className='text-xl leading-7 mb-4 text-neutral-600 dark:text-neutral-400'>{project.desc}</p>
+
+                                        <div className='flex flex-row align-bottom space-x-4'>
+                                            <div className='flex flex-wrap flex-row justify-start'>
+                                                {project.techstack.map((tech, techidx)=> {
+                                                    return(
+                                                        <p key={techidx} className="bg-gray-200 px-3 py-1.5 mb-1 mr-1 text-teal-600 text-sm rounded-full font-semibold"> {tech} </p>
+                                                    )
+                                                })}
+                                            </div>
+
+                                            <Link href={project.link} target="_blank">
+                                                <BsArrowUpRightSquare
+                                                size={30}
+                                                className="hover:-translate-y-1 transition-transform cursor-pointer"
+                                                />
+                                            </Link>
+                                        </div>
+                                    </div>
                                 </div>
-                            </div>
-                        </SlideUp>
-                    </div>
-                    )
-                })}
+                            </SlideUp>
+                        </div>
+                        )
+                    })}
+                </div>
             </div>
         </section>
     )
